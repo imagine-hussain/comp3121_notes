7ee63d89c915e9168d5d7d429e9ef074fe9f0dd1
imagine-hussain <hussain.nwz000@gmail.com>
2022-06-02 14:47:33 +1000

    add cleanup script

---
+++ cleanup_tex.sh
@@ -0,0 +1,15 @@
+# Running this script will clean up all tex build files
+rm *.aux *.lof *.log *.lot *.fls *.out *.toc *.fmt *.fot *.cb *.cb2 .*.lb;
+rm *.dvi
+rm *.xdv
+rm *-converted-to.*
+rm *.bbl
+rm *.bcf
+rm *.blg
+rm *-blx.aux
+rm *-blx.bib
+rm *.run.xml
+rm *.fdb_latexmk
+rm *.synctex
+rm *.synctex\(busy\)
+rm *.synctex.gz*
